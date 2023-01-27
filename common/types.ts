39e182f16899c4529613dfd28065f08d4eb90b02
Patrick Krueger <sendthistopatrick@gmail.com>
2023-01-27 16:21:37 -0700

    Back to the starting line

--- common/types.ts
+++ common/types.ts
@@ -1,3 +1,1 @@
-export interface Dog {
-  name: string;
-}
+
--- db/dbContext.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import Dog from "./models/Dog";
-
-class DbContext {
-  Dog = mongoose.model("Dog", Dog);
-}
-
-const dbContext = new DbContext();
-export default dbContext;
