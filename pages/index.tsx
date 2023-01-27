ee1a02af164c473bb35b8c3b8bfac9afe278f14d
Patrick Krueger <sendthistopatrick@gmail.com>
2023-01-26 23:37:10 -0700

    Format and linter config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,7 +12,8 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>
+        <h1 className="bg-red-800 text-center text-3xl text-emerald-500">Hello World!</h1>
       </main>
     </>
-  )
+  );
 }
